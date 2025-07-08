2490a51daaf48d9112a4325072f4e35100eaae0c
Sasi <sasikiran.tt3011@gmail.com>
2025-07-08 15:10:20 +0530

    Datatypes

---
+++ Datatypes.java
@@ -0,0 +1,36 @@
+/* Program for Data types in Java */
+
+public class Datatypes {
+    public static void main(String[] args) {
+        // Integer types
+        // Integer types
+        int i = 100;
+        short sh = 50;
+        long l = 123456789L;
+        byte b = 10;
+
+        // Floating point types
+        float f = 12.34f;
+        double d = 56.789;
+
+        // Character type
+        char ch = 'Z';
+
+        // Boolean type
+        boolean bool = true;
+
+        // String type (not a primitive, but commonly used)
+        String str = "Hello, Java!";
+
+        // Print values
+        System.out.println("int: " + i);
+        System.out.println("short: " + sh);
+        System.out.println("long: " + l);
+        System.out.println("byte: " + b);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("char: " + ch);
+        System.out.println("boolean: " + bool);
+        System.out.println("String: " + str);
+    }
+}
