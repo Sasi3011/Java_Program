9c77465c17bb502b730ea3812affff41be673d7b
Sasi <sasikiran.tt3011@gmail.com>
2025-07-08 15:07:25 +0530

    Added Basic programs

---
+++ Basic.java
@@ -0,0 +1,10 @@
+public class Basic{
+    public static void main(String[] args)
+    {
+        System.out.println("Name: Sasikiran");
+        System.out.println("Roll No: 24CS216");
+        System.out.println("Branch: Computer Science");
+        System.out.println("Section: D");
+        System.out.println("Year: II");
+    }
+}
---
+++ Input.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Input{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.print("Enter your roll number: ");
+        String rollNo = scanner.nextLine();
+        System.out.print("Enter your branch: ");
+        String branch = scanner.nextLine();
+        System.out.print("Enter your section: ");
+    }
+}
