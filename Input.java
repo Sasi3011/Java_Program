83d6c78952a62796ef861fd1f5d47984850c6c3b
Sasi <sasikiran.tt3011@gmail.com>
2025-07-09 14:46:37 +0530

    Changes Input

--- Input.java
+++ Input.java
@@ -2,13 +2,14 @@ import java.util.Scanner;
 public class Input{
     public static void main(String[] args)
     {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
-        System.out.print("Enter your roll number: ");
-        String rollNo = scanner.nextLine();
-        System.out.print("Enter your branch: ");
-        String branch = scanner.nextLine();
-        System.out.print("Enter your section: ");
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter your name: ");
+            String name = scanner.nextLine();
+            System.out.print("Enter your roll number: ");
+            String rollNo = scanner.nextLine();
+            System.out.print("Enter your branch: ");
+            String branch = scanner.nextLine();
+            System.out.print("Enter your section: ");
+        }
     }
 }
